250ba5d166bcb00951ef54719e3cf64ed393c669
ssunguh <stevesunguh2001@gmail.com>
2022-10-20 11:53:56 -0400

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	char *output = "_putchar\n";
+
+	while (*output)
+	{
+		_putchar(output);
+		output++;
+	}
+	return (0);
+}
