51ad073842afbd2deab79405083a4c7802908a8c
ssunguh <stevesunguh2001@gmail.com>
2022-10-20 12:54:27 -0400

    Nested loops with alphas

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
- * Return: Always 0 (Success)
+ * Return: x10 a-z
  */
 
 void print_alphabet_x10(void)
 {
 	int count = 0;
 
-    while (count < 10)
-    {
-        char letter = 'a';
+	while (count < 10)
+	{
+		char letter = 'a';
 
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-	    _putchar('\n');
-        count++;
-    }
-    
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		count++;
+	}
 }
