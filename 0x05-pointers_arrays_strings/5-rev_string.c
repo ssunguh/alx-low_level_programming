106daa6375e7c6d239c9820f32af50127f161007
ssunguh <stevesunguh2001@gmail.com>
2022-10-25 08:20:06 -0400

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a character
+ * Return: no return
+ */
+
+void rev_string(char *s)
+{
+	int count = 0;
+	char *first = &(s[0]);
+	
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	count--;
+
+	while (count >= 0)
+	{
+		if (*s != '\0')
+		{
+			*s = *(first + count);
+		}
+		else
+		{
+			*s += *(first + count);
+		}
+		count--;
+	}
+	*s += '\0';
+}
