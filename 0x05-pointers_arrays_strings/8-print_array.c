03688e8b30f269b2b086f389bf8fcdf5c6a89bf9
ssunguh <stevesunguh2001@gmail.com>
2022-10-25 10:30:34 -0400

    print arr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints out the second half of a string
+ * @a: pointer to an array of integers
+ * @n: size of the array
+ * Return: no return
+ */
+
+void print_array(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf('\n');
+}
